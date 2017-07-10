91ef9f6f992dc23a165f2e2d3873cd3a925fe410
Ashok <ashokcha@hcl.com>
2017-07-10 05:32:50 +0000

    Added motd recipe in git

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -4,14 +4,23 @@
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
 # Install apache package
-package 'httpd' do
-	package_name 'httpd'
+
+if node['platform_family'] == "rhel"
+	package = 'httpd'
+elseif
+   node['platform_family'] == "debian"
+	package = 'apache'
+end
+
+package 'apache2' do
+	package_name package
 	action :install
 end
 	
-serivce 'httpd' do
+service 'apache2' do
 	service_name 'httpd'
 	action [:start, :enable]
 end
 
 
+#include_recipe 'apache::websites'
